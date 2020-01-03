158cd669e74f8359b601b99ede5e87046a06d6bf
Andy Yan <andy@Andys-MacBook-Air.local>
2020-01-03 16:42:43 -0500

    we created a calculation checker

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var wrongLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func check(_ sender: UIButton) {
+        let answer = Int(answerTextField.text!)!
+        
+        if answer == 33 {
+            wrongLabel.isHidden = true
+            correctLabel.isHidden = false
+        } else {
+            correctLabel.isHidden = true
+            wrongLabel.isHidden = false
+        }
+    }
 }
-
