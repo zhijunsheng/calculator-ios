01a99f50bc0952eade8385c5e87154297386ee56
Angie Yan <angieyan@Andys-MacBook-Air.local>
2020-01-05 11:05:38 -0500

    I added a working check button and a label that displays if the answer's corrector incorrect. Also, I added an additional "generate" button to generate more numbers to solve the equation.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var number1Label: UILabel!
+    @IBOutlet weak var number2Label: UILabel!
+    
+    
+    @IBOutlet weak var checkmark: UILabel!
+    
+   
+    @IBAction func check(_ sender: Any) {
+        let x = Int(answerTextField.text!)!
+        let num1 = Int(number1Label.text!)!
+        let num2 = Int(number2Label.text!)!
+        let sum = num1 + num2
+        print("\(sum)")
+        
+        if x == sum {
+            checkmark.text = "Correct!"
+        } else {
+            checkmark.text = "Incorrect!"
+            
+        }
+    }
+    
+    @IBAction func generate(_ sender: Any) {
+        number1Label.text = "\(arc4random() % 10 + 1)"
+        number2Label.text = "\(arc4random() % 10 + 1)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-
+    
+   
+    
+    
 }
 
