8e928e7215e3e1a6b9c847dda884c5da0f01a6b2
Andy Yan <andy@Andys-MacBook-Air.local>
2020-01-10 16:33:08 -0500

    we fixed the problem of when you press the button it would crash

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,35 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var correctLabel: UILabel!
     @IBOutlet weak var wrongLabel: UILabel!
     @IBOutlet weak var number1Label: UILabel!
-    
+    @IBOutlet weak var number2Label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let a = Int(arc4random())
-        let smallerA = a % 99 + 1
-        number1Label.text = "\(smallerA)"
-        
-        
+        nextQuestion()
     }
     
     @IBAction func check(_ sender: UIButton) {
+        if answerTextField.text == ("") {
+            return
+        }
         let answer = Int(answerTextField.text!)!
-        if answer == Int(number1Label.text!)! + 10 {
+        if answer == Int(number1Label.text!)! + Int(number2Label.text!)! {
             wrongLabel.isHidden = true
             correctLabel.isHidden = false
         } else {
             correctLabel.isHidden = true
             wrongLabel.isHidden = false
         }
-        
     }
+    
     @IBAction func next(_ sender: UIButton) {
+        nextQuestion()
+    }
+    
+    func nextQuestion() {
         let a = Int(arc4random())
         let smallerA = a % 100
         number1Label.text = "\(smallerA)"
         
-        
+        let b = Int(arc4random())
+        let smallerB = b % 100
+        number2Label.text = "\(smallerB)"
     }
-    
 }
