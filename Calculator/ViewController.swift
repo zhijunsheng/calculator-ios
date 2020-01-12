1a067e9d95877e66a70a00b4744ba1c522e79925
Angie Yan <angieyan@Angies-MacBook-Pro.local>
2020-01-11 19:09:00 -0500

    I added more to the calculator project by adding an additional division problem following the same rules as the addition one.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var number1Label: UILabel!
     @IBOutlet weak var number2Label: UILabel!
     @IBOutlet weak var checkmark: UILabel!
+    @IBOutlet weak var quotientTextField: UITextField!
+    @IBOutlet weak var remainderTextField: UITextField!
+    @IBOutlet weak var number4Label: UILabel!
+    @IBOutlet weak var number3Label: UILabel!
+    @IBOutlet weak var questionmark: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +48,7 @@ class ViewController: UIViewController {
       
         checkmark.text = "?"
         answerTextField.text = ""
+
         startRandom()
     }
     
@@ -50,4 +56,37 @@ class ViewController: UIViewController {
         number1Label.text = "\(arc4random() % 100)"
         number2Label.text = "\(arc4random() % 100)"
     }
+    
+    
+    @IBAction func generateDivision(_ sender: Any) {
+        if quotientTextField.text == "" || questionmark.text == "Incorrect!" {
+                  return
+              }
+        questionmark.text = "?"
+        quotientTextField.text = ""
+        remainderTextField.text = ""
+        
+    }
+    
+    @IBAction func checkDivision(_ sender: Any) {
+        if quotientTextField.text == "" {
+            return
+        }
+        
+        let x = Int(quotientTextField.text!)!
+        let num1 = Int(number3Label.text!)!
+        let num2 = Int(number4Label.text!)!
+        let sum = num1 + num2
+               
+        if x == sum {
+            questionmark.text = "Correct."
+        } else {
+            questionmark.text = "Incorrect."
+        }
+        
+    }
+    
+    
 }
+
+
