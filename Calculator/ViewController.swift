dd8c642158970513f606e8e6778191b7bff53959
Andy Yan <andy@Andys-MacBook-Air.local>
2020-01-12 10:10:15 -0500

    we fixed a crash problem

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var answerTextField2: UITextField!
     @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var correctLabel2: UILabel!
     @IBOutlet weak var wrongLabel: UILabel!
+    @IBOutlet weak var wrongLabel2: UILabel!
     @IBOutlet weak var number1Label: UILabel!
     @IBOutlet weak var number2Label: UILabel!
+    @IBOutlet weak var secondNumber1Label: UILabel!
+    @IBOutlet weak var secondNumber2Label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +29,7 @@ class ViewController: UIViewController {
     @IBAction func check(_ sender: UIButton) {
         guard let rawText = answerTextField.text, let answer = Int(rawText) else {
             return
-        }        
+        }
         
         if answer == Int(number1Label.text!)! + Int(number2Label.text!)! {
             wrongLabel.isHidden = true
@@ -48,4 +53,33 @@ class ViewController: UIViewController {
         let smallerB = b % 100
         number2Label.text = "\(smallerB)"
     }
+    
+    @IBAction func check2(_ sender: Any) {
+        guard let rawText = answerTextField2.text, let answer = Int(rawText) else {
+            return
+        }
+        
+        if answer == Int(secondNumber1Label.text!)! / Int(secondNumber2Label.text!)! {
+            wrongLabel2.isHidden = true
+            correctLabel2.isHidden = false
+        } else {
+            correctLabel2.isHidden = true
+            wrongLabel2.isHidden = false
+        }
+    }
+    
+    @IBAction func next2(_ sender: UIButton) {
+        nextQuestion2()
+    }
+    
+    func nextQuestion2() {
+        let a = Int(arc4random())
+        let smallerA = a % 100
+        number1Label.text = "\(smallerA)"
+        
+        let b = Int(arc4random())
+        let smallerB = b % 100
+        number2Label.text = "\(smallerB)"
+    }
+    
 }
