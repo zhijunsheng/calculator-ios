d934bf0fc7fc9856eba9af938f2c3acee41dc8c8
Angie Yan <angieyan@Angies-MacBook-Pro.local>
2020-01-18 19:26:37 -0500

    I tested the second part of the project and found a bug where if you put the right remainder but the wrong quotient, it would still say "correct".

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startRandom1()
         startRandom()
     }
 
@@ -57,8 +57,17 @@ class ViewController: UIViewController {
         number2Label.text = "\(arc4random() % 100)"
     }
     
+    func startRandom1() {
+        number3Label.text = "\(arc4random() % 100)" // [0..99], not [1..100]
+        
+        // [1,2,3]
+        number4Label.text = "\(arc4random() % 99 + 1)"
+    }
+    
     
     @IBAction func generateDivision(_ sender: Any) {
+        startRandom1()
+        
         if quotientTextField.text == "" || questionmark.text == "Incorrect!" {
                   return
               }
@@ -69,24 +78,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func checkDivision(_ sender: Any) {
-        if quotientTextField.text == "" {
+        if quotientTextField.text == "" || remainderTextField.text == "" {
             return
         }
         
         let x = Int(quotientTextField.text!)!
+        let y = Int(remainderTextField.text!)!
         let num1 = Int(number3Label.text!)!
         let num2 = Int(number4Label.text!)!
-        let sum = num1 + num2
+        let quotient = num1 / num2
+        let remainder = num1 % num2
                
-        if x == sum {
+        if x == quotient {
+            questionmark.text = "Correct."
+        } else {
+            questionmark.text = "Incorrect."
+        }
+        
+        if y == remainder {
             questionmark.text = "Correct."
         } else {
             questionmark.text = "Incorrect."
         }
         
     }
-    
-    
+
+
 }
+
 
 
